15e9a69125604d4034a6e985643dcdf7f7a3aa83
ramanaditya <adityaraman96@gmail.com>
2021-02-11 21:45:05 +0530

    Static files dirs corrected

--- colossus/settings.py
+++ colossus/settings.py
@@ -131,7 +131,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'colossus/static'),
 ]
 
 
